f758a33623f41903753a7965f03eff96a647fdd3
vsiago <vsiago23@gmail.com>
2024-03-10 19:15:49 -0300

    melhor

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -17,7 +17,7 @@ export function ModeToggle({ mobile }: any) {
 
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger className="absolute  right-12" asChild>
+            <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
                     <SunIcon className="h-[1.2rem] w-[1.2rem] text-slate-600 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,45 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import { ModeToggle } from "./ModeToggle";
-import NavbarMobile from "../components/NavbarMobile";
 
 export default function Header() {
+    const [open, setOpen] = useState(false);
+
+    const menuItems = ["Home", "About", "Services", "Contact"];
+
     return (
-        <header className="w-full absolute z-[1000] top-0 bg-sky-500 text-sky-100 text-base">
-            <nav className="container mx-auto flex items-center justify-between py-4">
-                <Image
-                    src="/logo-proonco-clara.png"
-                    width={150}
-                    height={20}
-                    alt="Logo ProOnco"
-                />
-                {/* <ul className="flex gap-3">
-                    <li>
-                        <a className="px-3 py-2" href="/">
-                            Início
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#sobre">Sobre</a>
-                    </li>
-                </ul> */}
-                <div className="relative flex items-center">
-                    <ModeToggle />
-                    <NavbarMobile />
-                </div>
+        <header className="bg-slate-800 h-16 px-5 flex justify-between items-center">
+
+            <Image
+                src="/logo-proonco-clara.png"
+                width={150}
+                height={20}
+                alt="Logo ProOnco" />
+            <nav className="">
+                <ul
+                    onClick={() => setOpen(!open)}
+                    className="relative flex h-10 w-10 flex-col items-center justify-center gap-2 cursor-pointer hover:bg-slate-700 rounded transition duration-150 ease-in hover:transform hover:scale-105"
+                >
+                    <li
+                        className={`${open ? "transform rotate-45 absolute" : ""} w-6 h-[2px] rounded-[2px] bg-white transition duration-150 ease-in-out`}
+                    ></li>
+                    <li
+                        className={`${open ? "transform -rotate-45 absolute" : ""} w-6 h-[2px] rounded-[2px] bg-white transition duration ease-in-out`}
+                    ></li>
+                </ul>
+                <ul
+                    className={`${open ? "flex opacity-100 w-full transform translate-x-0" : "opacity-0 w-0 transform translate-x-[100%]"} absolute top-16 flex-1 right-0 flex flex-col transition duration-300 ease-out opacity-0 h-full bg-black/60`}
+                >
+                    {menuItems.map((item) => (
+                        <li
+                            className="odd:bg-slate-700/50 even:bg-slate-700/40 px-5 py-2 transition duration-500 ease-in hover:transform"
+                            key={item}
+                        >
+                            <a href={`${item}`}>{item}</a>
+                        </li>
+                    ))}
+                </ul>
             </nav>
         </header>
     );
